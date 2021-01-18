cd0de2da244e9aa188849f1edb95d9fb61647c26
damianu <damian.ulko@gmail.com>
2021-01-18 19:03:59 +0100

    Implement redux in HomeScreen.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router()
 // @desc Public
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
-
+    
     res.json(products)
 }))
 
