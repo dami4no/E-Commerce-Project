f37469e796bb7f8bd65c48ccb34758fb8e4469fe
damianu <damian.ulko@gmail.com>
2021-01-20 17:36:32 +0100

    Create productController in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,11 @@
 import express from 'express'
-import Product from '../models/productModel.js'
-import asyncHandler from 'express-async-handler'
-
 const router = express.Router()
-
-// @desc Fetch all products
-// @route GET /api/products
-// @desc Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    
-    res.json(products)
-}))
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 
 
-// @desc Fetch single product
-// @route GET /api/products/:id
-// @desc Public
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 
 
